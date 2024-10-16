808d1c6d01c18b333b0d2ceb586081f6624a87a9
RicardoBanuelos <ricardo.banuelos@uabc.edu.mx>
2024-10-15 20:10:15 -0700

    moved navbar into shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { NavbarModule } from './navbar/navbar.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,7 @@ import { NavbarModule } from './navbar/navbar.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavbarModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedComponent } from './shared.component';
+import { PagesRoutingModule } from '../pages/pages-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PagesRoutingModule
   ],
-  declarations: [SharedComponent]
+  exports: [NavbarComponent],
+  declarations: [NavbarComponent]
 })
 export class SharedModule { }
